6e0fe5bdffd58c92b08aedac904acc53d6041d84
Carlos Jiménez <caliche18a@gmail.com>
2023-01-26 12:58:46 -0500

    proyecto2

---
+++ ProyectosJavaBasico/Proyecto2/src/proyecto2/Main.java
@@ -0,0 +1,8 @@
+package proyecto2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenidos al proyecto 2 con múltiples ejercicios en Java");
+    }
+}
