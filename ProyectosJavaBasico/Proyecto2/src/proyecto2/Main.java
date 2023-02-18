32159a4545ebcf5320a467c2db9e8d8c0b676444
Carlos Jiménez <caliche18a@gmail.com>
2023-02-17 19:26:41 -0500

    Delete ProyectosJavaBasico/Proyecto2 directory

--- ProyectosJavaBasico/Proyecto2/src/proyecto2/Main.java
+++
@@ -1,8 +0,0 @@
-package proyecto2;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Bienvenidos al proyecto 2 con múltiples ejercicios en Java");
-    }
-}
